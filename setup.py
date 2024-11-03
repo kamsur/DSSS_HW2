36781f9e5abc224871503f8b065bfe703884251b
kamsur <57803389+kamsur@users.noreply.github.com>
2024-11-03 22:33:00 +0100

    Updated project structure for pip installation

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="math_quiz",
+    version="0.0.1",
+    author="Masudur Rahaman Kazi",
+    license="Apache 2.0",
+    long_description=long_description,
+    url="https://github.com/kamsur/DSSS_HW2.git"
+)
