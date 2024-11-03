3761ae1d3b35b5682e9d44d27f174bfe3c7f464c
kamsur <57803389+kamsur@users.noreply.github.com>
2024-11-03 23:13:07 +0100

    Updated setup.py

--- setup.py
+++ setup.py
@@ -5,5 +5,11 @@ setup(
     version="0.0.1",
     author="Masudur Rahaman Kazi",
     license="Apache 2.0",
-    url="https://github.com/kamsur/DSSS_HW2.git"
+    url="https://github.com/kamsur/DSSS_HW2.git",
+    packages=["math_quiz"],
+    entry_points={
+        'console_scripts': [
+            'math_quiz=math_quiz.math_quiz:math_quiz',
+        ],
+    },
 )
